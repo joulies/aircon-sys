f32fe10efd51145e4d36c2d0280068164ec3dcf9
Juliana Ausente <julianamarie.ausente@cvsu.edu.ph>
2026-05-28 17:23:36 +0800

    auq n

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,11 @@
+process.on("uncaughtException", (err) => {
+  console.error("🔥 UNCUGHT EXCEPTION:", err);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error("🔥 UNHANDLED REJECTION:", err);
+});
+
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
